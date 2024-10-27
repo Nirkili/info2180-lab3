91b8ecad1241c6b89ade4aff70d71f7f11979a16
Tara-Lee Donald <taralee.kd.db@gmail.com>
2024-10-27 14:57:57 -0500

    Finished code

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,51 +1,78 @@
-var state =[];
+var state =Array(9).fill('');
 var player = ['X','O'];
 var current = player[0];
 
 window.addEventListener('DOMContentLoaded', () => {
 
-    let board_ele = (document.getElementById('board')).children;
+    let board_ele = document.querySelectorAll('#board > div');
     let button = document.querySelector('.btn');
 
-    for(let i=0;i < board_ele.length; i++){
-        board_ele[i].classList.add('square');
+    board_ele.forEach((box,i) => {
+        box.classList.add('square');
 
-        board_ele[i].addEventListener('click', () => {
+        box.addEventListener('click', () => {
 
-            if(board_ele[i].textContent == ''){
-                board_ele[i].textContent = current;
-                board_ele[i].classList.add(current);
-                state.push(current);
-                console.log(state);
+            if(box.textContent == ''){
+                box.textContent = current;
+                box.classList.add(current);
+                state[i] = current;
+
+                Win_Check();
 
                 if(current == 'X'){
                     current = player[1]
-                    board_ele[i].classList.remove(current);
+                    box.classList.remove(current);
                 }
                 else{
                     current = player[0]
-                    board_ele[i].classList.remove(current);
+                    box.classList.remove(current);
                 }
             }
    
         });   
 
-        board_ele[i].addEventListener('mouseover', () => {
-            board_ele[i].classList.add('hover');
+        box.addEventListener('mouseover', () => {
+            box.classList.add('hover');
         });
 
-        board_ele[i].addEventListener('mouseout', () => {
-            board_ele[i].classList.remove('hover');
+        box.addEventListener('mouseout', () => {
+            box.classList.remove('hover');
+        });
+    })
+
+
+    button.addEventListener('click', () => {
+        board_ele.forEach((box, i) =>{
+            box.textContent = '';
+            box.classList.remove('X','O'); 
         });
 
-        button.addEventListener('click', () => {
-            board_ele[i].textContent = '';
-            board_ele[i].classList.remove('X','O');  
+        w = document.querySelector('#status');
+        p = document.querySelectorAll('#status > p');
+        w.classList.remove('you-won');
+
+        p.forEach((para) => {
+            para.remove();
         });
 
+        state =Array(9).fill('');
+        current = player[0];
+    });
+
+    function Win_Check(){
+        if(((state[0] === state[1] && state[1] === state[2] && state[0] !== '') ||(state[0] === state[4] && state[4] === state[8] && state[0] !== '') ||(state[0] === state[3] && state[3] === state[6] && state[0] !== '')) ||((state[3] === state[4] && state[4] === state[5] && state[4] !== '') ||(state[2] === state[4] && state[4] === state[6] && state[4] !== '') ||
+        (state[1] === state[4] && state[4] === state[7] && state[4] !== '')) ||((state[6] === state[7] && state[7] === state[8] && state[6] !== '') ||(state[2] === state[5] && state[5] === state[8] && state[8] !== ''))){
+            w = document.querySelector('#status');
+            w.classList.add('you-won');
+
+            p = document.createElement('p');
+
+            p.textContent = `Congratulations! ${current} has won!`
+
+            w.appendChild(p);
+            state =Array(9).fill('');
+        }
     }
-
-
 });
 
 
